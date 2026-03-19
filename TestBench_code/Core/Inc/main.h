7e90e571ff7cdf449e90dfcfe1f466718497ab20
Igor Burykin <rizz@DESKTOP-SG33N1V.>
2026-03-19 17:45:53 +0300

    dev

--- TestBench_code/Core/Inc/main.h
+++ TestBench_code/Core/Inc/main.h
@@ -61,6 +61,10 @@ void Error_Handler(void);
 #define START_MEASURE_BUTTON_GPIO_Port GPIOA
 #define MOTOR_M1_GATE_Pin GPIO_PIN_12
 #define MOTOR_M1_GATE_GPIO_Port GPIOA
+#define UART1_TX_Pin GPIO_PIN_6
+#define UART1_TX_GPIO_Port GPIOB
+#define UART1_RX_Pin GPIO_PIN_7
+#define UART1_RX_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- TestBench_code/Core/Src/main.c
+++ TestBench_code/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "i2c.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -88,6 +89,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
